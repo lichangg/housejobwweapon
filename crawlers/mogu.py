5dcf4d9ee56de9b88cba9bc506a0e8a9aec82b41
lichenggang <lcg@ichunt.com>
2019-07-26 14:35:21 +0800

    add mogu

--- crawlers/mogu.py
+++ crawlers/mogu.py
@@ -6,83 +6,97 @@ from base.gen_task import TaskProducer
 class BeikeCrawler(TaskProducer):
 
     def crawl(self, url_data):
+        area_pat = self.re.compile(r'(\d{1,5}\.\d)㎡') #TODO 得去掉这个
         ori_url = url_data.get('url')
         page = url_data.get('page')
-        url = ori_url+ f'/zufang/pg{page}'
+        city_id = url_data.get('city_id')
+
+
         headers = {
-            "Host": ori_url.replace('https://', ''),
-            "Referer": "https://sz.ke.com/ershoufang/",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"}
+            "Host": "api.mgzf.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0",
+            "Referer": "http://sz.mgzf.com/list/pg4/?searchWord=",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Channel": "3",
+            "UUID": "ad508977-0e95-7a5c-72a7-c93e5ebfb354",
+            "Content-Length": "49",
+            "Origin": "http://sz.mgzf.com",
+            "Connection": "keep-alive",
+        }
 
-        resp = self.requests.get(url, headers=headers)
-        dom = self.etree.HTML(resp.text)
-        house_doms = dom.xpath('//div[@class="content__list--item"]')
+        post_data = {
+            'currentPage': page,
+            'cityId': int(city_id),
+            'showCount': 18,
+            'searchWord': ''
+        }
+        domain = ori_url
+        url = 'https://api.mgzf.com/room-find-web/find/list'
+        resp = self.requests.post(url, headers=headers, data=post_data)
+        all_data = resp.json()
+        houses_data = all_data['content']['list']
         house_li = []
-        for house_dom in house_doms:
-            loc = house_dom.xpath('.//p[@class="content__list--item--des"]//a/text()')
-            title = house_dom.xpath('.//p[@class="content__list--item--title twoline"]//a/text()')
-            url = house_dom.xpath('.//p[@class="content__list--item--title twoline"]//a/@href')
-            rent = house_dom.xpath('.//span[@class="content__list--item-price"]//em/text()')
-            struct = house_dom.xpath('.//p[@class="content__list--item--des"]/text()')
-            attr = house_dom.xpath('.//p[@class="content__list--item--bottom oneline"]//i/text()')
-            thumb_img = house_dom.xpath('.//a[@class="content__list--item--aside"]//img/@data-src')
-            pub_time = house_dom.xpath('.//p[@class="content__list--item--time oneline"]/text()')
-
+        for house in houses_data:
             house_obj = dict()
-            house_obj['title'] = title[0].strip()
-            house_obj['house_url'] = ori_url + url[0]
-            house_obj['district'] = loc[0]
-            house_obj['loc'] = '-'.join(loc[1:])
-            house_obj['struct'] = '-'.join([i.strip() for i in struct]).strip('-')
-            house_obj['area'] = house_obj['struct'].split('-')[0]
-            house_obj['rent'] = rent[0]
-            house_obj['attr'] = attr
-            house_obj['community_name'] = loc[-1]
-            house_obj['thumb_img'] = thumb_img[0]
-            house_obj['pub_time'] = pub_time[0]
+            house_obj['title'] = house['title']
+            house_obj['house_url'] = domain + '/room/{}.shtml'.format(house['roomId'])
+            house_obj['district'] = house['title'].split('-')[0]
+            house_obj['loc'] = house['title']
+            house_obj['struct'] = house['subTitle']
+            house_obj['area'] = area_pat.findall(house['subTitle'])[0]
+            house_obj['rent'] = house['showPrice']
+            house_obj['attr'] = house['detailDesc'] + house['location']
+            house_obj['community_name'] = house['title'].split('-')[-1]
+            house_obj['thumb_img'] = house['pictureUrl']
+            house_obj['pub_time'] = ''
             house_obj['img_group'] = []
             house_obj['extra'] = None
             house_li.append(house_obj)
         return house_li
+
 if __name__ == '__main__':
-    #TODO 蘑菇的还没写
-    import requests
+
+    import requests,re
+    area_pat = re.compile(r'(\d{1,5}\.\d)㎡')
 
     headers = {
-               "Host": "mgzf.com",
-               "Referer": "https://sz.ke.com/ershoufang/",
-               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"}
-    domain='https://sz.zu.ke.com'
-    url = 'https://sz.zu.ke.com/zufang'
-    resp = requests.get(url,headers=headers)
-    from lxml import etree
-    dom = etree.HTML(resp.text)
-    house_doms = dom.xpath('//div[@class="content__list--item"]')
-    for house_dom in house_doms:
-        # city= dom.xpah
-        loc= house_dom.xpath('.//p[@class="content__list--item--des"]//a/text()')
-        title=house_dom.xpath('.//p[@class="content__list--item--title twoline"]//a/text()')
-        url=house_dom.xpath('.//p[@class="content__list--item--title twoline"]//a/@href')
-        # struct=house_dom.xpath('.//p[@class="content__list--item--des"]//a/text()')
-        rent=house_dom.xpath('.//span[@class="content__list--item-price"]//em/text()')
-        struct=house_dom.xpath('.//p[@class="content__list--item--des"]/text()')
-        attr=house_dom.xpath('.//p[@class="content__list--item--bottom oneline"]//i/text()')
-        thumb_img=house_dom.xpath('.//a[@class="content__list--item--aside"]//img/@data-src')
-        pub_time =house_dom.xpath('.//p[@class="content__list--item--time oneline"]/text()')
+        "Host": "api.mgzf.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0",
+        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "http://sz.mgzf.com/list/pg4/?searchWord=",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Channel": "3",
+        "UUID": "ad508977-0e95-7a5c-72a7-c93e5ebfb354",
+        "Content-Length": "49",
+        "Origin": "http://sz.mgzf.com",
+        "Connection": "keep-alive",
+    }
 
+    post_data={
+        'currentPage': 3,
+        'cityId': 340,
+        'showCount': 18,
+        'searchWord': ''
+    }
+    domain='http://sz.mgzf.com'
+    url = 'https://api.mgzf.com/room-find-web/find/list'
+    resp = requests.post(url,headers=headers,data=post_data)
+    all_data = resp.json()
+    houses_data=all_data['content']['list']
+    for house in houses_data:
         house_obj = dict()
-        # house_obj['city'] = city[0].replace('深圳', '')
-        house_obj['title'] = title[0].strip()
-        house_obj['house_url'] = domain + url[0]
-        house_obj['district'] = loc[0]
-        house_obj['loc'] = '-'.join(loc[1:])
-        house_obj['struct'] = '-'.join([i.strip() for i in struct]).strip('-')
-        house_obj['area'] = house_obj['struct'].split('-')[0]
-        house_obj['rent'] = rent[0]
-        house_obj['attr'] = attr
-        house_obj['community_name'] = loc[-1]
-        house_obj['thumb_img'] = thumb_img[0]
-        house_obj['pub_time'] = pub_time[0]
+        house_obj['title'] = house['title']
+        house_obj['house_url'] = domain+'/room/{}.shtml'.format(house['roomId'])
+        house_obj['district'] = house['title'].split('-')[0]
+        house_obj['loc'] = house['title']
+        house_obj['struct'] = house['subTitle']
+        house_obj['area'] = area_pat.findall(house['subTitle'])[0]
+        house_obj['rent'] = house['showPrice']
+        house_obj['attr'] = house['detailDesc']+house['location']
+        house_obj['community_name'] = house['title'].split('-')[-1]
+        house_obj['thumb_img'] = house['pictureUrl']
+        house_obj['pub_time'] = ''
         house_obj['img_group'] = []
         house_obj['extra'] = None
         print(house_obj)
