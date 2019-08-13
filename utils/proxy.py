f5f66b37899d4b48e001a6d8b1f4fe17b9389127
lichenggang <lcg@ichunt.com>
2019-08-13 14:00:00 +0800

    递归深度有问题，暂定1.0.0版本

--- utils/proxy.py
+++ utils/proxy.py
@@ -1,16 +1,2 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-proxyHost = "http-proxy-t3.dobel.cn"
-proxyPort = "9180"
-proxyUser = "LIEXINHTT1"
-proxyPass = "oK97IIl298"
-proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
-        "host" : proxyHost,
-        "port" : proxyPort,
-        "user" : proxyUser,
-        "pass" : proxyPass,
-}
-proxies = {
-        "http"  : proxyMeta,
-        "https" : proxyMeta,
-}
